e5a6c000aac9969566d776506abc5e54ef99206d
crazyoptimist <hey@crazyoptimist.net>
2023-06-27 11:25:08 -0500

    refactor: reconfigure nodemon

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,1 @@
-import { config } from 'dotenv';
-config();
-
 export const FOO = process.env.FOO || 'VAR';
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import { config } from 'dotenv';
+config();
+
 import { FOO } from './config';
 import { logger } from './utils/logger';
 
